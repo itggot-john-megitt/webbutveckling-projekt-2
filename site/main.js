e999266bca443fe29f7673e5ab5873469f6e049e
itggot-john-megitt <>
2019-01-27 23:08:23 +0100

    some changes, will add info later

--- site/main.js
+++ site/main.js
@@ -1,7 +1,20 @@
-function showMenu(event){
-    let menu = document.querySelector(".menu")
-    menu.classList.toggle("show")
+function showMenu(event){    
+    document.getElementById("mySidenav").style.width = "60vw";
 
-    let fadeLayer = document.querySelector(".fadeLayer")
-    fadeLayer.classList.toggle("visible")
+    document.getElementById("myFadeLayer").style.opacity = "0.3";
+
+
+    let menubutton2 = document.querySelector(".menubutton2")
+    menubutton2.classList.toggle("visibleMenubutton2")
 }
+
+function closeMenu(event){    
+    document.getElementById("mySidenav").style.width = "0";
+
+    document.getElementById("myFadeLayer").style.opacity = "0";
+
+    let menubutton2 = document.querySelector(".menubutton2")
+    menubutton2.classList.toggle("visibleMenubutton2")
+} 
+
+
